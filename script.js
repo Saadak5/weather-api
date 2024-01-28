59e186e9ef9867f93844274fc6c7a67b579d3645
Saadak Ahmed <saadak98@hotmail.com>
2024-01-28 22:29:13 +0200

    Scriptiin lisätty getweather funktio, jossa on Open Weather sivustosta generoitu api avain. funktiossa myös if- lause, joka näyttää viestin, jos haeuttua kaupunkia ei löydy

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //tähän koodia
+function getWeather() {
+    const apiKey = 'ffd8aedc95ef5a1f2e2d5391cce6cdde';
+    const city = document.getElementById('city').value;
+
+    if (!city) {
+        alert('Note: Enter a city');
+        return;
+    }
+}
