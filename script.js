ba25cea5e7b211278045431b109b95aafe029f8e
Saadak Ahmed <saadak98@hotmail.com>
2024-01-30 17:22:14 +0200

    Sää ennuste toimintoa ohjelmassa ei ole, joten sen div poistettu

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Function to get weather data
+//tähän koodia
 function getWeather() {
     const apiKey = 'ffd8aedc95ef5a1f2e2d5391cce6cdde';
     const city = document.getElementById('city').value;
@@ -13,19 +13,17 @@ function getWeather() {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Save the weather data to localStorage
             localStorage.setItem('weatherData', JSON.stringify(data));
 
-            // Call the function to present weather
             presentWeather(data);
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
             alert('Error fetching current weather data, try again please.');
         });
+
 }
 
-// Function to present weather
 function presentWeather(data) {
     const tempDiv = document.getElementById('temp');
     const infoDiv = document.getElementById('info');
@@ -43,28 +41,26 @@ function presentWeather(data) {
         const iconId = data.weather[0].icon;
         const iconUrl = `https://openweathermap.org/img/wn/${iconId}@4x.png`;
 
-        // Save additional weather data to localStorage
         localStorage.setItem('cityName', cityName);
         localStorage.setItem('temperature', temperature);
         localStorage.setItem('description', desc);
         localStorage.setItem('iconUrl', iconUrl);
-
+        
         const temperatureHTML = `<p>${temperature}°C</p>`;
-        const weatherHTML = `<p>${cityName}</p><p>${desc}</p>`;
+        
+        const weatherHTML = `<p>${cityName}</p>
+        <p>${desc}</p>`;
 
         tempDiv.innerHTML = temperatureHTML;
         infoDiv.innerHTML = weatherHTML;
         iconDiv.src = iconUrl;
+        
 
         showIcon();
     }
 }
 
-// Function to show icon
 function showIcon() {
     const iconDiv = document.getElementById('icon');
     iconDiv.style.display = 'block';
 }
-
-// Event listener for a button click (assuming you have a button with id 'getWeatherButton')
-document.getElementById('getWeatherButton').addEventListener('click', getWeather);
